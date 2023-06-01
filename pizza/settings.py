63ab129d51dc69915b4e188ba3a8b67e7a8d81a6
Fa1lenDevil <118052582+Fa1lenDevil@users.noreply.github.com>
2023-06-02 00:59:37 +0300

    added password reset

--- pizza/settings.py
+++ pizza/settings.py
@@ -99,3 +99,4 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 MEDIA_URL = '/image/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'image')
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
