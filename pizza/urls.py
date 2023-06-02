8dcd544f40d4936a43e60f98b01276dbf41973c0
Fa1lenDevil <118052582+Fa1lenDevil@users.noreply.github.com>
2023-06-02 13:11:34 +0300

    added registration

--- pizza/urls.py
+++ pizza/urls.py
@@ -7,5 +7,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('menu.urls')),
+    path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
